e2229643d18d2f2f077f11d3f0d5a98c862697dd
hmchilders <brengrad.heather@gmail.com>
2024-03-20 15:06:46 -0600

    dashboard

---
+++ Dashboard/utils.py
@@ -0,0 +1,174 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----           load libraries           ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from dask_jobqueue import PBSCluster
+from dask.distributed import Client
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----  server request to aid processing  ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def get_cluster(account,cores=30):    
+    """Spin up a dask cluster.
+
+    Keyword arguments:
+    account -- your account number, e.g. 'UCSB0021'
+    cores -- the number of processors requested
+
+    Returns:
+    client -- can be useful to inspect client.cluster or run client.close()
+    """
+
+    cluster = PBSCluster(
+    # The number of cores you want
+    cores=1,
+    # Amount of memory
+    memory='10GB',
+     # How many processes
+    processes=1,
+    # The type of queue to utilize (/glade/u/apps/dav/opt/usr/bin/execcasper)
+    queue='casper', 
+    # Use your local directory
+    local_directory = '$TMPDIR',
+    # Specify resources
+    resource_spec='select=1:ncpus=1:mem=10GB',
+    # Input your project ID here
+    account = account,
+    # Amount of wall time
+    walltime = '02:00:00',
+    )
+
+    # Scale up
+    cluster.scale(cores)
+    
+    # Setup your client
+    client = Client(cluster)
+
+    return client
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----     cluster reading function       ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#modify the function if you want to pass the parameter
+def read_all_simulations(var):
+    '''prepare cluster list and read to create ensemble(group of data)
+    use preprocess to select only certain dimension and a variable'''
+    
+    # read all simulations as a list
+    cluster_list= sorted(glob.glob('/glade/campaign/cgd/tss/projects/PPE/PPEn11_LHC/transient/hist/PPEn11_transient_LHC[0][0-5][0-9][0-9].clm2.h0.2005-02-01-00000.nc'))
+    cluster_list = cluster_list[1:]
+
+    def preprocess(ds, var):
+        '''using this function in xr.open_mfdataset as preprocess
+        ensures that when only these four things are selected 
+        before the data is combined'''
+        return ds[['lat', 'lon', 'time', var]]
+    
+    #read the list and load it for the notebook
+    ds = xr.open_mfdataset( cluster_list, 
+                                   combine='nested',
+                                   preprocess = lambda ds: preprocess(ds, var),
+                                   parallel= True, 
+                                   concat_dim="ens")
+    
+    return ds
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----     load data stored in casper     ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#-------Gridcell Landareas Data-----
+# reading, storing, subsetting
+landarea_file = '/glade/campaign/cgd/tss/projects/PPE/helpers/sparsegrid_landarea.nc'
+
+landarea_ds = xr.open_dataset(landarea_file)
+
+landarea = landarea_ds['landarea']
+
+
+#-------Dummy Parameter Data---------
+# x variable data for plotting
+df = pd.read_csv('/glade/campaign/asp/djk2120/PPEn11/csvs/lhc220926.txt',index_col=0)
+# the only dimension here is the 'member' aka file index id [LCH0001-500]
+# convert to data set
+params = xr.Dataset(df)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----     correct time-parsing bug       ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def fix_time(da):
+    
+    '''fix CESM monthly time-parsing bug'''
+    
+    yr0 = str(da['time.year'][0].values)
+    
+    da['time'] = xr.cftime_range(yr0,periods=len(da.time),freq='MS',calendar='noleap')
+    
+    return da
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----  weigh dummy landarea by gridcell  ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#------Weight Gridcells by Landarea---
+def weight_landarea_gridcells(da,landarea):
+
+    # weigh landarea variable by mean of gridcell dimension
+    return da.weighted(landarea).mean(dim = 'gridcell')
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----     weigh dummy data time dim      ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#------Weighted Averages by Time---
+def yearly_weighted_average(da):
+    # Get the array of number of days from the main dataset
+    days_in_month = da['time.daysinmonth']
+
+    # Multiply each month's data by corresponding days in month
+    weighted_month = (days_in_month*da).groupby("time.year").sum(dim = 'time')
+
+    # Total days in the year
+    days_per_year = days_in_month.groupby("time.year").sum(dim = 'time')
+
+    # Calculate weighted average for the year
+    return weighted_month / days_per_year
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ----    User Selected Plotting Funct    ----
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def subset_and_plot_cluster(param, var):
+    '''describe the relationship between the selected variable 
+    and parameter(s) between 2005-2010. output a
+    cluster plot averaged by year.'''
+
+    # Read in and wrangle user selected parameter cluster
+    param_avg  = params[param]
+
+    
+    # Read in and wrangle user selected variable cluster
+    da_v = read_all_simulations(var)
+    # feb. ncar time bug
+    da = fix_time(da_v)
+    # convert xr.ds to xr.da
+    da = da[var]
+    # weight gridcell dim by global land area
+    da_global = weight_landarea_gridcells(da, landarea)
+    # weight time dim by days in month
+    da_global_ann = yearly_weighted_average(da_global)
+    # take global avg for variable over year dimension
+    var_avg = da_global_ann.mean(dim='year')
+
+
+    # Plotting
+    plt.scatter(x=param_avg, y=var_avg, color = '#62c900ff', alpha = 0.8)
+    # Set plot labels and title
+    plt.xlabel(param)
+    plt.ylabel(var)
+    plt.title('2005-2010 Global Average')
+    # Show the plot
+    plt.show()
